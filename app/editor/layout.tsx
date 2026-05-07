2bb8eeb15119b8942349bcb30b3bc68f3ed2aded
Chinmoy Biswas <61724571+chinmoybiswas93@users.noreply.github.com>
2026-05-07 22:20:22 +0600

    removed test files

--- app/editor/layout.tsx
+++
@@ -1,7 +0,0 @@
-import { EditorChrome } from "@/components/editor/editor-chrome";
-
-export default function EditorLayout({
-  children,
-}: LayoutProps<"/editor">) {
-  return <EditorChrome>{children}</EditorChrome>;
-}
--- app/editor/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function EditorPage() {
-  return (
-    <div className="flex h-full items-center justify-center">
-      <span className="text-sm text-text-faint">Canvas</span>
-    </div>
-  );
-}
